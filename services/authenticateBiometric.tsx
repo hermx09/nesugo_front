1550de866e6185a5d7f87c9d0012190bf721eeb2
hermx09 <sui.nagayama@gmail.com>
2025-06-05 21:31:19 +0900

    顔認証修正

--- services/authenticateBiometric.tsx
+++ services/authenticateBiometric.tsx
@@ -2,20 +2,34 @@ import * as LocalAuthentication from 'expo-local-authentication';
 import { Alert } from 'react-native';
 
 const authenticateBiometric = async () => {
-  const compatible = await LocalAuthentication.hasHardwareAsync();
-  if (!compatible) {
-    return false;
-  }
 
-  const enrolled = await LocalAuthentication.isEnrolledAsync();
-  if (!enrolled) {
-    return false;
-  }
+    const supportResult = await LocalAuthentication.supportedAuthenticationTypesAsync();
+    alert(supportResult);
+
+    const compatible = await LocalAuthentication.hasHardwareAsync();
+    if (!compatible) {
+        Alert.alert("最初");
+        return false;
+    }
+
+    const enrolled = await LocalAuthentication.isEnrolledAsync();
+    if (!enrolled) {
+        Alert.alert("次");
+        return false;
+    }
 
   const result = await LocalAuthentication.authenticateAsync({
-    promptMessage: '生体認証でログイン',
-    fallbackLabel: 'パスコードを使う',
+    promptMessage: "認証を促すメッセージ",
+    cancelLabel: "キャンセルラベル",
+    fallbackLabel: "認証失敗時のメッセージ",
+    disableDeviceFallback: false,
   });
+  if (result.success) {
+    alert("認証成功");
+  } else {
+    LocalAuthentication.cancelAuthenticate();
+    alert("認証失敗");
+  }
 
   return result.success;
 };
